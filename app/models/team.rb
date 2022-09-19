e7f8eaf383344aaf664658f3b876596ba892a8df
en-Barry <en_barry@icloud.com>
2022-09-20 00:36:45 +0900

    add: 球団名を取得するスクレイピングを作成

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,22 @@
+require 'open-uri'
+require 'nokogiri'
+
 class Team < ApplicationRecord
   has_many :orders
   has_many :games, through: :orders
   has_many :players
 
-  validates :name, :short_name, presence: true
+  validates :name, presence: true, uniqueness: true
+
+  def self.get_teams
+    url = 'https://baseball.yahoo.co.jp/npb/teams/'  
+    html = URI.open(url).read
+    doc = Nokogiri::HTML.parse(html)
+
+    doc.css('.bb-teamHead__title').children.children.each do |team_name|
+      team = Team.new
+      team.name = team_name.text
+      team.save!
+    end
+  end
 end
